a5f07e358f41e0b33314541353baedacaf57b500
nicolas-dlp <49464264+nicolas-dlp@users.noreply.github.com>
2019-06-05 20:11:49 -0500

    Add files via upload

---
+++ Tec+Info.py
@@ -0,0 +1,30 @@
+#descargar procesador lenguaje
+import nltk
+nltk.download()
+#descargar fichero de chat
+pip install chatterbot
+#iniciar chatterbot
+from chtatterbot import ChatBot
+chatbot = ChatBot(
+    "Bot TecInfo"
+    trainer = "chatterbot.trainers.ChatterbotCorpusTrainer"
+)
+chatbot.train(
+    "chatterbot.corpus.spanish"
+)
+#el input debe iniciar con >
+while True:
+    usuario = input(">")
+    respuesta = chatbot.get_response(usuario)
+    print ("BOT "+str(respuesta))
+#debe entrenarse con conceptos necesarios, entonces
+Chatbot = ChatBot(
+    "Bot TecInfo"
+    trainer = "chatterbot.trainers.ListTrainers")
+    chatbot.train([
+        "hola"
+        "¿cuál es el precio?"
+        "¿cual es el precio?"
+        "precio?
+    ])
+    
